7b7babdecf3ea5ec98c46c2298ca4e42fab988a1
komputerkerja <komputerkerja100@gmail.com>
2020-06-28 03:05:03 +0700

    test

---
+++ Views_On_Progress/1. Auth(Login_Reg_Forgot)/auth.js
@@ -0,0 +1,46 @@
+const login = document.querySelector('nav ul li:nth-child(1)');
+const regist = document.querySelector('nav ul li:nth-child(2)');
+const forgot = document.querySelector('nav ul li:nth-child(3)');
+const frmlogin = document.querySelector('section:nth-child(2)');
+const frmregist = document.querySelector('section:nth-child(3)');
+const frmforgot = document.querySelector('section:nth-child(4)');
+const viewP = document.querySelectorAll('form .input-group .password');
+
+login.addEventListener('click',()=>{
+    login.classList.add('active');
+    regist.classList.remove('active');
+    forgot.classList.remove('active');
+
+    frmlogin.classList.remove('form-hidden');
+    frmregist.classList.remove('form-show');
+    frmforgot.classList.remove('form-show');
+});
+regist.addEventListener('click',()=>{
+    login.classList.remove('active');
+    regist.classList.add('active');
+    forgot.classList.remove('active');
+
+    frmlogin.classList.add('form-hidden');
+    frmregist.classList.add('form-show');
+    frmforgot.classList.remove('form-show');
+});
+forgot.addEventListener('click',()=>{
+    login.classList.remove('active');
+    regist.classList.remove('active');
+    forgot.classList.add('active');
+
+    frmlogin.classList.add('form-hidden');
+    frmregist.classList.remove('form-show');
+    frmforgot.classList.add('form-show');
+});
+
+viewP.forEach((e)=>{
+    e.addEventListener('mousedown', ()=>{
+        let pElement = e.parentElement.children[0];
+        pElement.type = 'text';
+    });
+    e.addEventListener('mouseup', ()=>{
+        let pElement = e.parentElement.children[0];
+        pElement.type = 'password';
+    });
+});
