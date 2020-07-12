e93a26c51d236858a8f359036873a9916c4d6068
komputerkerja <komputerkerja100@gmail.com>
2020-07-12 12:51:09 +0700

    test

--- ApplicationTemplates/00GABUNGAN_TEST/index.php
+++ ApplicationTemplates/00GABUNGAN_TEST/index.php
@@ -3,6 +3,6 @@
 require_once 'config/config.php';
 require_once 'Templates/header.php';
 require_once 'Navigator/navigator.php';
-require_once 'Pembelian/pembelian.php';
+require_once 'Penjualan/penjualan.php';
 require_once 'Templates/footer.php';
 
