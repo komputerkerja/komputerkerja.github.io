bdd55338bb2a1d7ca911918bce87fbff9c379b56
komputerkerja <komputerkerja100@gmail.com>
2021-07-22 00:36:19 +0700

    OK

--- TestLab/05.PHP_RestAPI_Test/index.php
+++ TestLab/05.PHP_RestAPI_Test/index.php
@@ -5,7 +5,3 @@ $conn = new PDO('mysql:host=localhost;dbname=dbnews','root','');
 $stmt = $conn->prepare('SELECT * FROM posts');
 
 $stmt->execute();
-
-while($row = fetch(PDO::FETCH_ASSOC){
-		var_dump($row);
-}
